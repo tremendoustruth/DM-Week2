07887f4441969340ea515d0a1e8429ac2ccc075d
Bushra Salama <bushra.salama@gmail.com>
2022-10-26 10:53:42 -0500

    revised

--- challenge-has-unique-char/hasUniqueChars.js
+++ challenge-has-unique-char/hasUniqueChars.js
@@ -10,12 +10,19 @@ function hasUniqueChars(string){
 }
 
 let word = "OneHundred"
-const uniqueCount = (str) => {
-    let is_equal = false;
-    let set_str = new Set(str);
-    if (set_str.size === str.length)
-        is_equal = true;
-    return is_equal;
+// const uniqueCount = (str) => {
+//     let is_equal = false;
+//     let set_str = new Set(str);
+//     if (set_str.size === str.length)
+//         is_equal = true;
+//     return is_equal;
+// }
+// console.log(uniqueCount(word));
+// hasUniqueChars(sampleString);
+
+function hasUniqueChars2(string){
+    let setStr = new Set(string)
+    if(setStr.size===string.length){return true}
+    else {return false}
 }
-console.log(uniqueCount(word));
-hasUniqueChars(sampleString);
+console.log(hasUniqueChars2(word))
